c94afaa8c6854d621f0710281785e0be53ded29b
HFERARD <ferard2006@gmail.com>
2025-10-31 19:32:14 +0100

    Travail personnel 31/10

--- add.cpp
+++ add.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, char *argv []) {
-    int sum = 0;
-    for (int i=0; i < argc; i++)
+
+int main(int argc, char* argv []){
+    if (argc == 1) 
+    // Un seul argument --> nom du programme 
     {
-        sum = sum + std::atoi(argv[i]); 
+        std::cerr << "not enough arguments" << std::endl;
+        return -1;
+    } else 
+    // Sinon on somme les arguments suivants
+    {
+        int sum = 0;
+        for (int i = 1; i < argc; i++)
+        {
+            sum = sum + std::stoi(argv[i]);
+        }
+        std::cout << sum << std::endl;
+        return 0;
     }
-    std::cout << sum << std::endl;
-    return 0;
 }
